0367411470a2e40f1b6eb2ed2c164c1e84c83fbc
Rafael Herdani <147459978+RafaelHerdani@users.noreply.github.com>
2025-06-02 00:49:20 +0700

    bagian 3 - Checkpoint 1

---
+++ w08Firebase101-multiplecollection/app/src/main/java/com/alexwawo/w08firebase101/Student.kt
@@ -0,0 +1,11 @@
+package com.alexwawo.w08firebase101
+
+data class Student(
+    val docId: String = "",
+    val id: String = "",
+    val name: String = "",
+    val program: String = "",
+    val phones: List<String> = emptyList()
+)
+
+
